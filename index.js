bf1c994d737c520a37c29295c0f6c7051e1cb148
Ashini98K <ashini.kulatunga@gmail.com>
2022-05-15 19:35:26 +0530

    Did changes to index file

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello SLIIT Mith Raveena Vibuda hey");
 });
 
+app.get("/output", (req, res) => {
+  res.send("Struggle solved :)");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
